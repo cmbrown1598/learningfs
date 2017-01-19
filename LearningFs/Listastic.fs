222dc59d69e54f4ca9280c66a6a0f550950739f2
Christopher Brown <cbrown@paraport.com>
2017-01-19 13:37:05 -0800

    List stuff

---
+++ LearningFs/Listastic.fs
@@ -0,0 +1,28 @@
+﻿module Listastic
+
+let length list = 
+    let rec loop acc =        
+        match acc with
+        | _::tail -> 
+            1 + loop (tail)
+        | [] -> 0
+    loop list
+
+let rev list = 
+    let rec loop lst acc = 
+        match lst with
+            |[] -> acc
+            |head::tail -> loop tail (head::acc)
+    match list with
+        | [] -> 
+            []
+        | [x] -> 
+            [x]
+        | a::b::tail -> 
+            loop tail [b;a]
+
+
+    
+    
+
+    
